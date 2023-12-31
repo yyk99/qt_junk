20f392cfeac8f675d5e578d4aed12fde469f4eab
Yuri Y. Kuznetsov <yyk@kestrel>
2023-12-31 13:43:51 -0500

    Add ammeraal/3.4/

---
+++ ammeraal/3.4/hlinesmain.cpp
@@ -0,0 +1,11 @@
+// hlinesmain.cpp: Hidden-line
+//    elimination; main function.
+#include "hlines.h"
+int main(int argc, char* argv[]) {
+    QApplication a(argc, argv);
+    HLines w;
+    w.show();
+    return a.exec();
+}
+
+// Ammeraal, Leen.Computer Graphics Programming in C++ / Qt(p. 138).UNKNOWN.Kindle Edition.
