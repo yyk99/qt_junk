95be0fb4ce1dc25853f325e168ceb2c01776a0fe
Yuri Y. Kuznetsov <yyk@kestrel>
2024-01-21 11:43:37 -0500

    Update qt_app3/qt_app3_ru_RU.ts

--- qt_app3/qt_app3_ru_RU.ts
+++ qt_app3/qt_app3_ru_RU.ts
@@ -1,3 +1,13 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ru_RU"></TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build-qt5-vs2022/qt_app3_autogen/include_Debug/ui_mainwindow.h" line="51"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
