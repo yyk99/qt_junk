172dfc5bfdb14044d185f95bbef37b61c00392be
Yura K <yyk@silvana.home>
2024-10-06 07:09:13 -0400

    Update q_app2/q_app2_en_US.ts

--- q_app2/q_app2_en_US.ts
+++ q_app2/q_app2_en_US.ts
@@ -5,61 +5,51 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="87"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="28"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="97"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="39"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="98"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="49"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="88"/>
         <source>New</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="52"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="90"/>
         <source>New window</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="57"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="92"/>
         <source>Open</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="62"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="93"/>
         <source>Save</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="67"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="94"/>
         <source>Save As</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="72"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="95"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="77"/>
-        <location filename="build-vs2022-x64/q_app2_autogen/include_Debug/ui_mainwindow.h" line="96"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
