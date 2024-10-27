f8fe16629f2c4c28000b76f486481f625598d463
Yury Kuznetsov <yyk99@kestrel.home>
2024-10-27 13:02:50 -0400

    Add more project on image loading

---
+++ q_load_image_in_scene/load_image_in_scene.cpp
@@ -0,0 +1,22 @@
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
+
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QGraphicsScene scene;
+    auto pixmap = QPixmap(":images/Carina2-Nielsen.jpg");
+    qDebug() << pixmap.size();
+    QGraphicsPixmapItem *item = new QGraphicsPixmapItem(pixmap);
+    scene.addItem(item);
+
+    QGraphicsView view(&scene);
+    view.show();
+
+    return a.exec();
+}
