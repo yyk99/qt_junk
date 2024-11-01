1a71e7552df593e8d7165ae3b05612b9105d5827
Yury Kuznetsov <yyk99@kestrel.home>
2024-11-01 19:05:49 -0400

    Add some button slots

--- qt_tab_control/tab_dialog.cpp
+++ qt_tab_control/tab_dialog.cpp
@@ -5,11 +5,15 @@
 #include "tab_dialog.h"
 #include "./ui_tab_dialog.h"
 
+#include <QDebug>
+
 TabDialog::TabDialog(QDialog *parent)
     : QDialog(parent)
     , ui(new Ui::TabDialog)
 {
     ui->setupUi(this);
+
+    qDebug() << "m_tabWidget.count" << ui->tabWidget->count();
 }
 
 TabDialog::~TabDialog()
@@ -17,3 +21,16 @@ TabDialog::~TabDialog()
     delete ui;
 }
 
+void
+TabDialog::on_connect_btn_clicked()
+{
+    qDebug() << "on_connect_btn_clicked...";
+    qDebug() << "Current:" << ui->tabWidget->currentWidget()->windowTitle();
+    qDebug() << "current index:" << ui->tabWidget->currentIndex();
+}
+
+void
+TabDialog::on_add_source_btn_clicked()
+{
+    qDebug() << "on_add_source_btn_clicked...";
+}
