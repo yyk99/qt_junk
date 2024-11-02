859b4b60d3d0daa842b04f64cc2410eba9c99290
Yury Kuznetsov <yyk99@kestrel.home>
2024-11-02 06:36:29 -0400

    Update

--- qt_tab_control/tab_dialog.cpp
+++ qt_tab_control/tab_dialog.cpp
@@ -27,10 +27,18 @@ TabDialog::on_connect_btn_clicked()
     qDebug() << "on_connect_btn_clicked...";
     qDebug() << "Current:" << ui->tabWidget->currentWidget()->windowTitle();
     qDebug() << "current index:" << ui->tabWidget->currentIndex();
+
+    qDebug() << "Current Tab text:" << ui->tabWidget->tabText(ui->tabWidget->currentIndex());
 }
 
 void
 TabDialog::on_add_source_btn_clicked()
 {
     qDebug() << "on_add_source_btn_clicked...";
+    QWidget *tab = ui->tabWidget->currentWidget();
+    if (auto lst  = tab->findChild<QListView *>("listView"))
+    {
+        qDebug() << "Found!";
+
+    }
 }
