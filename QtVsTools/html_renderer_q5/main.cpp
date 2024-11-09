b8ee85811ac7e9e1e736441ea77cb69e00079939
yyk99 <yyktomtom@gmail.com>
2024-11-09 16:42:52 -0500

    Add QtVsTools/html_renderer_q5 (#7)
    Co-authored-by: Yuri Kuznetsov <yyk@plein-2>
    Co-authored-by: Yury Kuznetsov <yyk99@kestrel.home>

---
+++ QtVsTools/html_renderer_q5/main.cpp
@@ -0,0 +1,20 @@
+//
+//
+//
+
+#include "html_renderer_q5.h"
+#include "../common/DebuggingConsole.h"
+
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+#if _DEBUG
+    attachDebugConsole();
+#endif
+
+    QApplication a(argc, argv);
+    html_renderer_q5 w;
+    w.show();
+    return a.exec();
+}
