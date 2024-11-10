952f404155e97a58153a8d40bf7014e55917b0ad
Yuri Kuznetsov <yyk@plein-2>
2024-11-10 10:00:59 -0500

    Add cmake-configure-all.sh

--- README.txt
+++ README.txt
@@ -1,4 +1,8 @@
 cmake configure & build
 =======================
 
-	"c:\Program Files\CMake\bin\cmake.exe" -S . -B build-qt5-vs2022 -DCMAKE_PREFIX_PATH=c:/qt/5.15.2/msvc2019_64/lib/cmake
+$ "c:\Program Files\CMake\bin\cmake.exe" -S . -B build-qt5-vs2022 -DCMAKE_PREFIX_PATH=c:/qt/5.15.2/msvc2017_64/lib/cmake
+
+or
+
+$ "c:\Program Files\CMake\bin\cmake.exe" -S . -B build-qt5-vs2022 -DCMAKE_PREFIX_PATH=c:/qt6/5.14.2/msvc2017_64/lib/cmake
--- qc-proj1/qc-proj1_en_US.ts
+++ qc-proj1/qc-proj1_en_US.ts
@@ -5,19 +5,16 @@
     <name>AboutDialog</name>
     <message>
         <location filename="aboutdialog.ui" line="14"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_aboutdialog.h" line="44"/>
         <source>Dialog</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="26"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_aboutdialog.h" line="45"/>
         <source>About...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="aboutdialog.ui" line="39"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_aboutdialog.h" line="46"/>
         <source>Qt Version will be here</source>
         <translation type="unfinished"></translation>
     </message>
@@ -26,52 +23,60 @@
     <name>ImageGridDialog</name>
     <message>
         <location filename="imagegriddialog.ui" line="14"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_imagegriddialog.h" line="35"/>
-        <source>Dialog</source>
+        <source>Image Grid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="imagegriddialog.ui" line="21"/>
+        <location filename="imagegriddialog.ui" line="26"/>
+        <location filename="imagegriddialog.ui" line="31"/>
+        <location filename="imagegriddialog.ui" line="36"/>
+        <source>New Row</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="imagegriddialog.ui" line="41"/>
+        <location filename="imagegriddialog.ui" line="46"/>
+        <location filename="imagegriddialog.ui" line="51"/>
+        <location filename="imagegriddialog.ui" line="56"/>
+        <source>New Column</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_mainwindow.h" line="80"/>
-        <source>MainWindow</source>
+        <location filename="mainwindow.ui" line="20"/>
+        <source>Main Window ...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="28"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_mainwindow.h" line="85"/>
+        <location filename="mainwindow.ui" line="34"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="37"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_mainwindow.h" line="86"/>
+        <location filename="mainwindow.ui" line="43"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="47"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_mainwindow.h" line="81"/>
+        <location filename="mainwindow.ui" line="53"/>
         <source>Select Online Source</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="52"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_mainwindow.h" line="82"/>
+        <location filename="mainwindow.ui" line="58"/>
         <source>Exit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="57"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_mainwindow.h" line="83"/>
+        <location filename="mainwindow.ui" line="63"/>
         <source>Image Grid</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="62"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_mainwindow.h" line="84"/>
+        <location filename="mainwindow.ui" line="68"/>
         <source>About...</source>
         <translation type="unfinished"></translation>
     </message>
@@ -80,38 +85,86 @@
     <name>OnlineSourceSheet</name>
     <message>
         <location filename="onlinesourcesheet.ui" line="14"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_onlinesourcesheet.h" line="90"/>
-        <source>Dialog</source>
+        <source>Online Source Selector</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="onlinesourcesheet.ui" line="26"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_onlinesourcesheet.h" line="91"/>
         <source>Tab 1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="onlinesourcesheet.ui" line="31"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_onlinesourcesheet.h" line="98"/>
+        <location filename="onlinesourcesheet.ui" line="33"/>
+        <location filename="onlinesourcesheet.ui" line="86"/>
+        <source>123</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="38"/>
+        <location filename="onlinesourcesheet.ui" line="91"/>
+        <source>456</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="43"/>
+        <location filename="onlinesourcesheet.ui" line="96"/>
+        <source>789</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="52"/>
         <source>Tab 2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="onlinesourcesheet.ui" line="44"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_onlinesourcesheet.h" line="93"/>
+        <location filename="onlinesourcesheet.ui" line="61"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="66"/>
+        <source>Second</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="71"/>
         <source>c1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="onlinesourcesheet.ui" line="49"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_onlinesourcesheet.h" line="95"/>
+        <location filename="onlinesourcesheet.ui" line="76"/>
         <source>c2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="onlinesourcesheet.ui" line="54"/>
-        <location filename="build-vs2022/qc-proj1_autogen/include_Debug/ui_onlinesourcesheet.h" line="97"/>
+        <location filename="onlinesourcesheet.ui" line="81"/>
         <source>c3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="101"/>
+        <source>One</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="106"/>
+        <source>Two</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="111"/>
+        <source>Three</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="121"/>
+        <location filename="onlinesourcesheet.ui" line="148"/>
+        <source>CheckBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="onlinesourcesheet.ui" line="128"/>
+        <source>...</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
