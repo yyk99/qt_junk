2d5f07916c2f1940ef2d2beb00a1c45364ab6cef
Yury Kuznetsov <yyk99@kestrel.home>
2024-11-23 15:34:26 -0500

    Add custom headers

--- table_view/table_view/tablemodel.h
+++ table_view/table_view/tablemodel.h
@@ -18,6 +18,10 @@ public:
     int rowCount(const QModelIndex &parent) const;
     int columnCount(const QModelIndex &parent) const;
     QVariant data(const QModelIndex &index, int role) const;
+
+    // QAbstractItemModel interface
+public:
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
 };
 
 #endif // TABLEMODEL_H
