e9c4733b206c568f2b2b4669021931bed58532e8
Yury Kuznetsov <yyk99@kestrel.home>
2024-11-24 09:23:56 -0500

    Rename Dialog to MainDialog

--- table_view/table_view/main.cpp
+++ table_view/table_view/main.cpp
@@ -1,11 +1,12 @@
-#include "dialog.h"
+#include "main_dialog.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Dialog w;
+    MainDialog w;
+    w.setWindowTitle("Table View Dialog");
     w.show();
     return a.exec();
 }
